ec5736427968d933908b58aaa7c5c7f58698613c
Eshasalunkhe <esalunkhe158@gmail.com>
2025-06-15 02:56:52 +0530

    pushed the updated files

---
+++ Iterators/changetoUpperCase.js
@@ -0,0 +1,8 @@
+const names = ["James", "Jane", "Doe", "Roni"];
+
+const upperCaseNames = names.map((element) => {
+    return element.toUpperCase();
+});
+
+console.log(upperCaseNames); 
+
---
+++ Iterators/example.js
@@ -0,0 +1,9 @@
+const names = ["James", "Jane", "Doe", "Roni"];
+
+const printArray = (names) => {
+    names.forEach(name => {
+        console.log(name);
+    });
+};
+
+printArray(names);
